783098f5c2125be44115c9a2b168fdc715c852a2
Jean-Yves Boudreau <jy.boudreau@gmail.com>
2015-11-27 14:51:29 -0500

    Express skeleton

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
-
 var express = require('express');
 var bodyParser = require('body-parser');
+
+var app = express();
+
+// Add a JSON body parser middleware..
+app.use(bodyParser.json());
+
+// Serve our public folder statically.
+app.use(express.static('public'));
+
+// Start the application.
+var server = app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server listening on port ${server.address().port}`);
+});
